1622a919032ef26b71baf178f6eda0e7a11650b9
Lalit Rajak <lalitrajak312@gmail.com>
2024-12-21 05:55:54 +0530

    using mongodb database

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { RootModule } from './root.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(RootModule);
-  await app.listen(process.env.PORT ?? 5000);
+  const PORT = process.env.PORT ?? 3000;
+  await app.listen(PORT);
+  console.info('Info: Application running on PORT', PORT);
 }
 bootstrap();
