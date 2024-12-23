71fa38d7cd6239bb8b77fc30826e4a0bdc224c48
Lalit Rajak <lalitrajak312@gmail.com>
2024-12-23 17:49:28 +0530

    updated cors policy

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { RootModule } from './root.module';
-import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(RootModule);
-  app.use(
-    cors({
-      origin: ['http://localhost:3000', 'https://hyperhireui.vercel.app/'],
-    }),
-  );
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://hyperhireui.vercel.app'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+  });
   const PORT = process.env.PORT ?? 3000;
   await app.listen(PORT);
   console.info('Info: Application running on PORT', PORT);
